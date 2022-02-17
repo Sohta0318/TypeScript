8bd6f0c35723ad4a9794a3367f2a0051b1010a21
SOHTA0318 <nicosohta0318@gmail.com>
2022-02-16 22:40:15 -0800

    finish sec13

---
+++ sec 13 3rd Party Libraries/src/app.ts
@@ -0,0 +1,37 @@
+// import _ from "lodash";
+// console.log(_.shuffle([1, 2, 3]));
+
+// declare var GLOBAL: any;
+// console.log(GLOBAL);
+import { validate } from "class-validator";
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import { Product } from "./product.model";
+
+const products = [
+  { title: "Carpet", price: 29.99 },
+  { title: "A Book", price: 10.99 },
+];
+
+const newProd = new Product("", -5.99);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("VALIDATION ERRORS");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
+// const p1 = new Product("A Book", 12.99);
+
+// const loadedProducts = products.map(
+//   (prod) => new Product(prod.title, prod.price)
+// );
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
+
+// console.log(p1.getInformation());
